f1f3723fab8cbf12e05207a1a8620ddcce084b99
Daishiro Nishida <daishironishida@gmail.com>
2023-10-14 23:01:00 +0900

    ctoiで手間取ったのでもう１回

--- 231014/d.cpp
+++ 231014/d.cpp
@@ -2,23 +2,6 @@
 
 using namespace std;
 
-
-int ctoi(char c) {
-	switch (c) {
-		case '0': return 0;
-		case '1': return 1;
-		case '2': return 2;
-		case '3': return 3;
-		case '4': return 4;
-		case '5': return 5;
-		case '6': return 6;
-		case '7': return 7;
-		case '8': return 8;
-		case '9': return 9;
-		default: return 0;
-	}
-}
-
 bool checkPerm(long a, unordered_map<int, int> counts, int n) {
     for (int i = 0; i < n; i++) {
         int digit = a % 10;
@@ -42,19 +25,19 @@ int main() {
     // get min
     long min = 0;
     for (int i = 0; i < N; i++) {
-        min += ctoi(s[N-i-1]) * pow(10, i);
+        min += int(s[N-i-1] - '0') * pow(10, i);
     }
 
     // get max
     long max = 0;
     for (int i = 0; i < N; i++) {
-        max += ctoi(s[i]) * pow(10, i);
+        max += int(s[i] - '0') * pow(10, i);
     }
 
     // dict
     std::unordered_map<int, int> counts;
     for (int i = 0; i < N; i++) {
-        counts[ctoi(s[i])]++;
+        counts[int(s[i]) - '0']++;
     }
 
     // sqmin/max
