3ecc15a757fd8d4f1f600097ac05b7b554cbd1a6
Daishiro Nishida <daishironishida@gmail.com>
2023-10-21 15:39:32 +0900

    p2通った

--- pgbattle/2023/p2.cpp
+++ pgbattle/2023/p2.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-long long eval(string& s, int index, int q_count) {
+double eval(string& s, int index, int q_count) {
     if (s[index] != 'A' && s[index] != '?') {
         return 0;
     }
-    if (s[index+1] != 'B' && s[index] != '?') {
+    if (s[index+1] != 'B' && s[index+1] != '?') {
         return 0;
     }
-    if (s[index+2] != 'C' && s[index] != '?') {
+    if (s[index+2] != 'C' && s[index+2] != '?') {
         return 0;
     }
 
@@ -19,7 +19,7 @@ long long eval(string& s, int index, int q_count) {
             q_other--;
         }
     }
-    return pow(3, q_other);
+    return pow(3, q_other - q_count);
 }
 
 int main() {
@@ -33,11 +33,10 @@ int main() {
         }
     }
 
-    long long sum = 0;
+    double sum = 0;
     for (int i = 0; i < s.size() - 2; i++) {
         sum += eval(s, i, q_count);
     }
 
-    double result = (double)sum / pow(3, q_count);
-    cout << setprecision(10) << result;
+    cout << setprecision(10) << sum;
 }
