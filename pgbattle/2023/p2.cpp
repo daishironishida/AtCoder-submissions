595b0c9f34b324b89902152f9ca098734231bfb9
Daishiro Nishida <daishironishida@gmail.com>
2023-10-21 15:47:09 +0900

    きれいにしてみた

--- pgbattle/2023/p2.cpp
+++ pgbattle/2023/p2.cpp
@@ -2,40 +2,37 @@
 
 using namespace std;
 
-double eval(string& s, int index, int q_count) {
-    if (s[index] != 'A' && s[index] != '?') {
+bool isValid(char query, char truth) {
+    return query == '?' || query == truth;
+}
+
+double eval(string& s, int index) {
+    if (!isValid(s[index], 'A')) {
         return 0;
     }
-    if (s[index+1] != 'B' && s[index+1] != '?') {
+    if (!isValid(s[index+1], 'B')) {
         return 0;
     }
-    if (s[index+2] != 'C' && s[index+2] != '?') {
+    if (!isValid(s[index+2], 'C')) {
         return 0;
     }
 
-    int q_other = q_count;
+    int q_count = 0;
     for (int i = index; i < min(index + 3, (int)s.size()); i++) {
         if (s[i] == '?') {
-            q_other--;
+            q_count++;
         }
     }
-    return pow(3, q_other - q_count);
+    return pow(3, -q_count);
 }
 
 int main() {
     string s;
     cin >> s;
 
-    int q_count = 0;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '?') {
-            q_count++;
-        }
-    }
-
     double sum = 0;
     for (int i = 0; i < s.size() - 2; i++) {
-        sum += eval(s, i, q_count);
+        sum += eval(s, i);
     }
 
     cout << setprecision(10) << sum;
