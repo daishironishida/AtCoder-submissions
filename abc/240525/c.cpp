e7da128ac39496d12ba2da31fe1df73b60654683
Daishiro Nishida <daishironishida@gmail.com>
2024-05-25 21:28:41 +0900

    submit

--- abc/240525/c.cpp
+++ abc/240525/c.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
     int N, T;
-    cin >> N, T;
+    cin >> N >> T;
     vector<int> count(2 * N + 2);
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 1; i < T+1; i++) {
         int a;
         cin >> a;
 
@@ -15,22 +15,28 @@ int main() {
         int col = a / N;
 
 
-        if (count.at(row)++ == N) {
+        if (++count.at(row) == N) {
             cout << i;
+            return(0);
         }
-        if (count.at(N + col)++ == N) {
+        if (++count.at(N + col) == N) {
             cout << i;
+            return(0);
         }
         if (row == col) {
-            if (count.at(2*N)++ == N) {
+            if (++count.at(2*N) == N) {
                 cout << i;
+                return(0);
             }
         } 
-        if ((N-row) == col) {
-            if (count.at(2*N+1)++ == N) {
+        if ((N-row-1) == col) {
+            if (++count.at(2*N+1) == N) {
                 cout << i;
+                return(0);
             }
         }
+
+
     }
 
     cout << -1;
