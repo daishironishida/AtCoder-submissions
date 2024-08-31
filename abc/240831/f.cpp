3e6b531b568dd1ec6355956ca4ab64e8d87d0b3d
Nishida Daishiro <nishidadaishiro@team-lab.com>
2024-08-31 17:20:03 +0400

    loop back at the end

--- abc/240831/f.cpp
+++ abc/240831/f.cpp
@@ -27,9 +27,9 @@ int main() {
         map.at(r-1).at(c-1) = true;
     }
 
-    unordered_map<pair<int, int>, pair<int, string>, pair_hash> rewards;
+    unordered_map<pair<int, int>, pair<int, char>, pair_hash> rewards;
     int value = map.at(0).at(0) ? 1 : 0;
-    rewards[make_pair(0, 0)] = make_pair(value, "");
+    rewards[make_pair(0, 0)] = make_pair(value, 'x');
 
     for (int i = 1; i < H + W - 1; i++) {
         for (int r = 0; r < H; r++) {
@@ -41,38 +41,49 @@ int main() {
             int sum = 0;
 
             char direction;
-            string str;
             if (r == 0) {
                 direction = 'R';
                 sum = rewards[make_pair(r, c-1)].first;
-                str = rewards[make_pair(r, c-1)].second;
             } else if (c == 0) {
                 direction = 'D';
                 sum = rewards[make_pair(r-1, c)].first;
-                str = rewards[make_pair(r-1, c)].second;
             } else {
                 int rVal = rewards[make_pair(r-1, c)].first;
                 int cVal = rewards[make_pair(r, c-1)].first;
                 if (rVal > cVal) {
                     direction = 'D';
                     sum = rVal;
-                    str = rewards[make_pair(r-1, c)].second;
                 } else {
                     direction = 'R';
                     sum = cVal;
-                    str = rewards[make_pair(r, c-1)].second;
                 }
             }
 
             int current = map.at(r).at(c) ? 1 : 0;
             sum += current;
-            str += direction;
 
-            rewards[make_pair(r, c)] = make_pair(sum, str);
+            rewards[make_pair(r, c)] = make_pair(sum, direction);
         }
     }
 
     cout << rewards[make_pair(H-1, W-1)].first << endl;
-    cout << rewards[make_pair(H-1, W-1)].second << endl;
+
+    vector<char> routeBackwards;
+    routeBackwards.reserve(H + W - 1);
+    int r = H-1;
+    int c = W-1;
+    for (int i = 1; i < H + W - 1; i++) {
+        char direction = rewards[make_pair(r, c)].second;
+        routeBackwards.push_back(direction);
+        if (direction == 'R') {
+            c--;
+        } else {
+            r--;
+        }
+    }
+
+    for (int i = routeBackwards.size() - 1; i >= 0; i--) {
+        cout << routeBackwards.at(i);
+    }
 
 }
